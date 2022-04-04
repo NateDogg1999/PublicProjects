33bb137bc5938070315215a8b502a411f0bd2436
Nathan Loos <loosnathan1@gmail.com>
2022-04-04 11:58:35 -0700

    Changed ChromeDriver to work with multiple install locations

--- Project Source Code/IndeedScraper/Functions/ChromeDriver.py
+++ Project Source Code/IndeedScraper/Functions/ChromeDriver.py
@@ -9,49 +9,54 @@ import sys
 
 #Gets the version of chrome installed
 def get_version_via_com(filename):
-    parser = Dispatch("Scripting.FileSystemObject")
-    try:
-        version = parser.GetFileVersion(filename)
-    except Exception:
-        return None
-    return version
+	parser = Dispatch("Scripting.FileSystemObject")
+	try:
+		version = parser.GetFileVersion(filename)
+	except Exception:
+		return "11"
+	return version
 
 def launchDriver():
-    paths = [r"C:\Program Files\Google\Chrome\Application\chrome.exe",
-            r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
-    version = list(filter(None, [get_version_via_com(p) for p in paths]))[0]
+	paths = [r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+		  r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
+	version = list(filter(None, [get_version_via_com(p) for p in paths]))[0]
+	
+	#Creates a random fake 'useragent' to reduce the amount of Captchas
+	ua = UserAgent()
+	userAgent = ua.random
+	
+	
+	#Headless is disabled because I love watching it work
+	chrome_options = Options()
+	#chrome_options.add_argument("--headless")
+	username = os.getlogin( )
+	chrome_options.add_argument(f'user-agent={userAgent}')
+	
+	
+	try:
+		#Makes the chromedriver use Canary instead of normal chrome to reduce the amount of Captchas
+		chrome_options.binary_location = "C:\\Users\\" + username + r"\AppData\Local\Google\Chrome SxS\Application\chrome.exe"
+		driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriverCanary"), chrome_options=chrome_options)
 
-    #Creates a random fake 'useragent' to reduce the amount of Captchas
-    ua = UserAgent()
-    userAgent = ua.random
-
-    
-    #Headless is disabled because I love watching it work
-    chrome_options = Options()
-    #chrome_options.add_argument("--headless")
-    username = os.getlogin( )
-    chrome_options.add_argument(f'user-agent={userAgent}')
-
-
-    try:
-        #Makes the chromedriver use Canary instead of normal chrome to reduce the amount of Captchas
-        chrome_options.binary_location = "C:\\Users\\" + username + r"\AppData\Local\Google\Chrome SxS\Application\chrome.exe"
-        driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriverCanary"), chrome_options=chrome_options)
-    except:
-        #If canary fails, use normal chrome instead
-	    if version[1] != '8' and version[1] != '9' and version[1] != '0':
-		    input("Make sure you have version 98 or higher of Google Chrome installed. Press enter to exit the program.")
-		    sys.exit()
-	    try:
-		    chrome_options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-		    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver" + version[1]), chrome_options=chrome_options)
-	    except:
-		    input("Google chrome does not seem to be installed in the default directory. Press enter to exit the program.")
-		    sys.exit()
+	#If canary fails, use normal chrome instead
+	except:
+		if version[1] != '8' and version[1] != '9' and version[1] != '0':
+			input("Make sure you have version 98 or higher of Google Chrome installed. Press enter to exit the program.")
+			sys.exit()
+		try:
+			chrome_options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+			driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver" + version[1]), chrome_options=chrome_options)
+		except:
+			try:
+				chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+				driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver" + version[1]), chrome_options=chrome_options)
+			except:
+				input("Google chrome does not seem to be installed in the default directory. Press enter to exit the program.")
+				sys.exit()
 
 
-    driver.delete_all_cookies()
-    return driver
+	driver.delete_all_cookies()
+	return driver
 
 
 
